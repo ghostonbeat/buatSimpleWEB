68ca38c23071259075e2ad59992e6795711b8a95
Syabana Minggus <86134024+ghostonbeat@users.noreply.github.com>
2021-07-19 09:42:01 +0700

    Update simpleweb.py

--- simpleweb.py
+++ simpleweb.py
@@ -1,35 +1,46 @@
-from os import *
-from touch import *
-from shutil import *
+import os
+import touch
+import shutil
 
-path = getcwd()
-folder = ['/proyekweb', '/proyekweb/component', '/proyekweb/content']
-file =  [folder[0]+'/index.php', folder[1]+'/Header.html', folder[1]+'/Footer.html', folder[2]+'/style.scss', folder[2]+'/javascript.js']
-pohon = path+folder[0]
+path = './'
+folder = ['proyekweb', 'proyekweb/component', 'proyekweb/content']
+bahan =  [folder[0]+'/index.php', folder[0]+'/test.html', folder[1]+'/Header.html', folder[1]+'/Footer.html', folder[2]+'/style.scss', folder[2]+'/javascript.js']
+akar = path+folder[0]
 
 def buatFolder():
     for i in range(len(folder)):
-        mkdir(path+folder[i])
+        os.mkdir(path+folder[i])
 
 def buatKomponen():
-    for i in range(len(file)):
-        touch(path+file[i])
+    for i in range(len(bahan)):
+        touch.touch(path+bahan[i])
 
 def hapusLama():
-    rmtree(path+folder[0])
+    shutil.rmtree(path+folder[0])
+    
+def pohon():
+    akr = []
+    for root in os.walk(akar):  
+        akr.append(root)
+
+    for i in range(len(akr)):
+        print(akr[i])
+
+    print(" "+folder[0])
+    for i in range(len(akr[0][1])):
+        print("|")
+        print("+--- "+akr[0][1][i])
+        for j in range(len(akr[1+i][2])):
+            print("|   |")
+            print("|   +--- "+str(akr[1+i][2][j]))
+    for i in range(len(akr[0][2])):
+        print("|")
+        print("+--- "+akr[0][2][i])
 
 def main():
     buatFolder()
     buatKomponen()
-    try:
-        walk(pohon)
-    except:
-        print("Mohon maaf, anda belum memiliki program 'Tree'")
-        print("Silahkan install dulu program 'Tree' pada Linux OS anda!")
-        print("Ubuntu/Debian -> sudo apt install tree")
-        print("Fedora/Red Hat -> sudo dnf install tree")
-        print("Arch Linux -> sudo pacman -S tree")
-    """Catatan : Pastikan sistem Linux Distro memiliki program Tree"""
+    pohon()
     print("paket proyek berhasil dibuat!! selamat mengerjakan!!")
 
 if __name__ == '__main__':
